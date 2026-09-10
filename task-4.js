8114c30b5cea7a99f2f55f3313938800dbfefa53
Maxim Poz <executioner2126@gmail.com>
2026-09-10 15:07:01 +0900

    task4 animate

--- task-4.js
+++ task-4.js
@@ -1,24 +1,52 @@
-let menu = document.querySelector('.menu')
-let position = 0
-let flag = true
+//Бургер меню
+function burger() {
+    let menu = document.querySelector('.menu')
+    let position = 0
+    let flag = true
 
-menu.addEventListener('click', function animate() {
-    if (flag) {
-        position++
-        menu.style.transform = `translateX(${position}px)`
-        if (position < 150) {
-            requestAnimationFrame(animate)
-        } else {
-            flag = false
-        }
+    menu.addEventListener('click', function animate() {
+        if (flag) {
+            position++
+            menu.style.transform = `translateX(${position}px)`
+            if (position < 150) {
+                requestAnimationFrame(animate)
+            } else {
+                flag = false
+            }
 
-    } else {
-        position--
-        menu.style.transform = `translateX(${position}px)`
-        if (position > 0) {
-            requestAnimationFrame(animate)
         } else {
-            flag = true
+            position--
+            menu.style.transform = `translateX(${position}px)`
+            if (position > 0) {
+                requestAnimationFrame(animate)
+            } else {
+                flag = true
+            }
         }
+    })
+}
+ burger()
+
+
+//Кубик
+let square = document.querySelector('.square')
+
+let moveSquare = square.animate(
+    [
+        {transform: "translateX(0)"},
+        {transform: "translateX(350px)"},
+        {transform: "translateX(0)"},
+    ],
+    {
+        duration: 5000
     }
+
+)
+
+moveSquare.pause()
+
+const btnPlay = document.querySelector('#btnSquare')
+
+btnPlay.addEventListener("click", ()=>{
+    moveSquare.play()
 })
