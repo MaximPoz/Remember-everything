62adc39a0f07469754aa641ef63ae6815303be55
Maxim Poz <executioner2126@gmail.com>
2026-09-14 17:38:31 +0900

    add_constructor_BD

---
+++ bd.js
@@ -0,0 +1,48 @@
+class User {
+    constructor({ name, email }) {
+        this.name = name;
+        this.email = email;
+    }
+
+    informList() {
+        return `Имя пользователя: ${this.name}, email: ${this.email}`
+    }
+}
+
+function formatMessage(users) {
+    let usersList = document.getElementById('usersList')
+    usersList.innerHTML = '';
+
+    users.forEach(user => {
+        const listItem = document.createElement('li')
+        listItem.textContent = user.informList()
+        usersList.append(listItem)
+    });
+}
+
+
+function init() {
+    let users = []
+    let btnCreatUser = document.querySelector(".usersButton")
+
+    btnCreatUser.addEventListener("click", () => {
+        let nameEL = document.querySelector("input[type='text']")
+        let emailEL = document.querySelector("input[type='email']")
+
+        let name = nameEL.value;
+        let email = emailEL.value;
+
+        if (name && email) {
+            let user = new User({name, email})
+            users.push(user)
+            formatMessage(users)
+
+            nameEL.value = ''
+            emailEL.value = ''
+        }
+    })
+
+    formatMessage(users)
+}
+
+init()
